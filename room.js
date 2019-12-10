dffec8ce4a5badabaf3ae308e77b90f88e2529ae
LeoRoma <xiajtn@gmail.com>
2019-12-10 11:51:57 +0000

    passed input from text into variables

--- room.js
+++ room.js
@@ -1,14 +1,29 @@
-const fs = require('fs');
+var fs = require('fs');
+var input = fs.readFileSync('input.txt').toString().split("\n");
+console.log(input);
 
-var txt = fs.readFileSync('./input.txt', 'utf8');
-console.log(txt);
+var roomDimension = input[0].split(' ');
+var hooverPosition = input[1].split(' ');
+var dirtPosition = input.slice(2, 5);
+var drivingInstructions = input.slice(-1)[0];
 
+
+// console.log(roomDimension);
+// for (i in input) {
+//   if(input[i].indexOf(' ') >= 0){
+//     coordinates.push(input[i]);
+//   } else {
+//     drivingInstructions.push(input[i]);
+//   };
+//   console.log(input[i]);
+// }
 
 class Room {
   constructor() {
-    this.coordinates = {};
-    this.hooverPosition = {};
-    this.dirtPosition = {};
+    this.roomDimension = roomDimension;
+    this.hooverPosition = hooverPosition;
+    this.dirtPosition = dirtPosition;
+    this.drivingInstructions = drivingInstructions;
   }
 
 
